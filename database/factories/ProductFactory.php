3772548a4af7b39dc561162fce9b8246242267ae
AmerShadow <amersohailroot@gmail.com>
2019-08-30 03:32:16 +0530

    Database Factories created and Factories seeded

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -7,6 +7,10 @@ use Faker\Generator as Faker;
 
 $factory->define(Product::class, function (Faker $faker) {
     return [
-        //
+        'name'           => $faker->word,
+        'desciption'    => $faker->paragraph,
+        'price'          => $faker->numberBetween(10,10000),
+        'discount'       => $faker->numberBetween(3.00,30.00),
+        'stock'          => $faker->numberBetween(0,100),
     ];
 });
