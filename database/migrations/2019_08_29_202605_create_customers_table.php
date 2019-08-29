3c8f4d681385011a4d2adc19e76b260be93daf45
AmerShadow <amersohailroot@gmail.com>
2019-08-30 02:36:39 +0530

    Migrations are created for Customers, Products and Reviews

--- database/migrations/2019_08_29_202605_create_customers_table.php
+++ database/migrations/2019_08_29_202605_create_customers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProductsTable extends Migration
+class CreateCustomersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,12 @@ class CreateProductsTable extends Migration
      */
     public function up()
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('customers', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->text('desciption');
-            $table->double('price');
-            $table->double('discount');
-            $table->integer('stock');
+            $table->string('address');
+            $table->string('email');
+            $table->string('mobile');
             $table->timestamps();
         });
     }
@@ -31,6 +30,6 @@ class CreateProductsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('customers');
     }
 }
